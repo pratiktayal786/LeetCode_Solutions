c9bf0b6eba76a473d11fbd5d86c7d6210d9f7d43
Pratik Tayal <77280501+pratiktayal786@users.noreply.github.com>
2022-12-10 12:10:59 +0530

    Time: 15 ms (88.62%), Space: 15.9 MB (54.01%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -5,27 +5,37 @@ class Solution
         {
 
             int n = nums.size();
-            sort(nums.begin(), nums.end());
-            int prev = nums[0];
-            int count = 0;
             vector<int> ans;
-            for (auto i: nums)
-            {
-                if (prev == i) count++;
-                else
-                {
-                    if (count > (n / 3))
-                    {
-                        ans.push_back(prev);
-                    }
-                    prev = i;
-                    count = 1;
+            
+            int c1 = 0, c2 = 0;
+            int n1 = -1, n2 = -1;
+            
+            for(int i = 0; i < n; i++){
+                if(nums[i] == n1) c1++;
+                else if(nums[i] == n2) c2++;
+                else if(c1 == 0){
+                    n1 = nums[i];
+                    c1 = 1;
+                }
+                else if(c2 == 0){
+                    n2 = nums[i];
+                    c2 = 1;
+                }
+                else{
+                    c1--;
+                    c2--;
                 }
             }
-            if (count > (n / 3))
-            {
-                ans.push_back(prev);
+            
+            c1 = c2 = 0;
+            
+            for(auto i : nums){
+                if(i == n1) c1++;
+                else if(i == n2) c2++;
             }
+            if(c1 > (n/3)) ans.push_back(n1);
+            if(c2 > (n/3)) ans.push_back(n2);
+            
             return ans;
         }
 };
