28df10137af21fdbce24d9330c5623fb8697f589
Pratik Tayal <77280501+pratiktayal786@users.noreply.github.com>
2022-12-17 20:18:49 +0530

    Time: 78 ms (31.33%), Space: 22.4 MB (56.61%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -5,19 +5,16 @@ class Solution
         {
 
             int n = nums.size();
-            int l = 0, r = n - 1;
-            int ans = 0;
-
-            if (n == 1) return nums[0];
+            int l = 0, r = n - 2;
+    
             while (l <= r)
             {
                 int mid = (l + r) / 2;
-                if (mid + 1 < n && nums[mid] == nums[mid + 1])
+                if (nums[mid] == nums[mid^1])
                 {
-                    mid++;
+                    l = mid+1;
                 }
-                if ((mid - l + 1) % 2 == 0) l = mid + 1;
-                else r = mid - 2;
+                else r = mid-1;
             }
             return nums[l];
         }
