42d9534c02535b6c48375ffe6081839fcd0be832
Pratik Tayal <77280501+pratiktayal786@users.noreply.github.com>
2023-01-23 13:49:24 +0530

    Time: 4 ms (37.69%), Space: 5.9 MB (71.60%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,20 +1,12 @@
 class Solution {
 public:
-    
-    double solve(double x, int n){
-        if(n <= 0) return 1.0; 
-        
-        if(n%2){
-            return (double)x * (double)solve(x, n-1);
-        }
-        return (double)solve(x*x, n/2);
-    }
     double myPow(double x, int n) {
-        int nn = n;
-        if(nn < 0) nn = (nn == INT_MIN) ? INT_MAX : nn*(-1);
-        double ans = solve(x, nn);
-        if(n < 0 && x < 0 && n%2==0) return (double)-1.0/(double)ans;
-        else if(n < 0) return (double)1.0/(double)ans;
-        return ans;
+        
+        if(n == 0) return 1;
+        double t = myPow(x, n/2);
+        if(n%2){
+            return n < 0 ? 1/x*t*t : x*t*t;
+        }
+        return t*t;
     }
 };
