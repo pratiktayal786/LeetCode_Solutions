60236ce20a339e839ec1c37d1699a94fe001e0c0
Pratik Tayal <77280501+pratiktayal786@users.noreply.github.com>
2023-01-30 12:35:10 +0530

    Time: 3 ms (83.32%), Space: 13.7 MB (45.18%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -2,31 +2,19 @@ class Solution {
 public:
     
     void solve(string str, int lp, int rp, vector<string> &ans){
-        
-        if(lp == 0 && rp != 0){
-            while(rp--){
-                str.push_back(')');
-            }
-        }
-        if(lp <= 0 && rp <= 0){
+      
+        if(lp == 0 && rp == 0){
             ans.push_back(str);
             return;
         }
-        if((lp != 0 && rp <= 0) || (lp > rp)){
-            return;
-        }
-        str.push_back('(');
-        solve(str, lp-1, rp, ans);
-        str.pop_back();
-        str.push_back(')');
-        solve(str, lp, rp-1, ans);
-        str.pop_back();
+        
+        if(lp > 0) solve(str+"(", lp-1, rp+1, ans);
+        if(rp > 0) solve(str+")", lp, rp-1, ans);
     }
     vector<string> generateParenthesis(int n) {
         
-        if(n == 0) return {""};
         vector<string> ans;
-        solve("(", n-1, n, ans);
+        solve("", n, 0, ans);
         return ans;
     }
 };
