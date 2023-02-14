505b1b83a3f0f07356c65cb9aa6e20bb76b6b015
Pratik Tayal <77280501+pratiktayal786@users.noreply.github.com>
2023-02-14 10:24:47 +0530

    Time: 2224 ms (9.54%), Space: 51.1 MB (39.10%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,23 +1,33 @@
-class Solution {
-public:
-    int longestConsecutive(vector<int>& nums) {
-        
-        unordered_set<int> s;
-        for(auto i : nums){
-            s.insert(i);
-        }
-        int maxy = 0;
-        for(auto i : nums){
-            if(!s.count(i-1)){
-                int curNum = i;
-                int count = 1;
-                while(s.count(curNum+1)){
-                    curNum++;
-                    count++;
-                }
-                maxy = max(maxy, count);
+class Solution
+{
+    public:
+        int longestConsecutive(vector<int> &nums)
+        {
+
+            set<int> s;
+
+            for (auto i: nums)
+            {
+                s.insert(i);
             }
+
+            int n = nums.size();
+            int maxStreak = 0;
+            for (auto i: nums)
+            {
+                if (!s.count(i - 1))
+                {
+                    int curNum = i;
+                    int curStreak = 1;
+                    while (s.count(curNum + 1))
+                    {
+                        curStreak++;
+                        curNum++;
+                    }
+                    maxStreak = max(maxStreak, curStreak);
+                }
+            }
+
+            return maxStreak;
         }
-        return maxy;
-    }
 };
