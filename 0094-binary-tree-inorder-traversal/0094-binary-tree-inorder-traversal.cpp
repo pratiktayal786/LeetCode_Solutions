ffc313437dd338a9b67c8646c2f10acd886e4c1f
Pratik Tayal <77280501+pratiktayal786@users.noreply.github.com>
2023-05-05 23:37:32 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (43.28%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -1,35 +1,51 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *    int val;
+ *    TreeNode * left;
+ *    TreeNode * right;
+ *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
  */
-class Solution {
-public:
-    vector<int> inorderTraversal(TreeNode* root) {
-        
-        vector<int> ans;
-        if(root == NULL) return ans;
-        
-        stack<TreeNode*> stk;
+class Solution
+{
+    public:
+        vector<int> inorderTraversal(TreeNode *root)
+        {
 
-        while(root || !stk.empty()){
-            
-            while(root){
-                stk.push(root);
-                root = root->left;
+            vector<int> ans;
+            if (!root) return ans;
+
+            while (root)
+            {
+                if (!root->left)
+                {
+                    ans.push_back(root->val);
+                    root = root->right;
+                }
+                else
+                {
+                    TreeNode *temp = root->left;
+
+                    while (temp->right && temp->right != root)
+                    {
+                        temp = temp->right;
+                    }
+                    if (temp->right == root)
+                    {
+                        temp->right = NULL;
+                        ans.push_back(root->val);
+                        root = root->right;
+                    }
+                    else
+                    {
+                        temp->right = root;
+                        root = root->left;
+                    }
+                }
             }
-            root = stk.top();
-            stk.pop();
-            ans.push_back(root->val);
-            root = root->right;
-            
+            return ans;
         }
-        return ans;
-    }
 };
