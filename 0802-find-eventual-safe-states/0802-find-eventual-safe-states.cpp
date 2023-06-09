38db3458a080cd07feec5510363bc92bd213cf1b
Pratik Tayal <77280501+pratiktayal786@users.noreply.github.com>
2023-06-09 10:00:27 +0530

    Time: 156 ms (88.86%), Space: 47.3 MB (83.88%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -3,7 +3,6 @@ public:
     
     bool dfs(int i, vector<int> &vis, vector<vector<int>> &graph, vector<int> &ans){
         vis[i] = 1;
-        
         for(auto j : graph[i]){
             if((vis[j] == 0 && dfs(j, vis, graph, ans)) || vis[j] == 1) return true;
         }
@@ -14,13 +13,13 @@ public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         
         int n = graph.size();
-        vector<int> ans;
         vector<int> vis(n, 0);
+        vector<int> ans;
         
         for(int i = 0; i < n; i++){
-            if(vis[i] == 0) dfs(i, vis, graph, ans);
+            if(vis[i] == 0) dfs(i, vis, graph, ans); 
         }
-        sort(ans.begin(),ans.end());
+        sort(ans.begin(), ans.end());
         return ans;
     }
 };
