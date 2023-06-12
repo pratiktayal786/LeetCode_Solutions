2e353ea061a797222229bdb4d2bf5b100f2023fd
Pratik Tayal <77280501+pratiktayal786@users.noreply.github.com>
2023-06-12 12:40:36 +0530

    Time: 38 ms (25.83%), Space: 15.5 MB (16.13%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,34 +1,30 @@
 class Solution {
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int K) {
-          // create adjacency list
-        unordered_map<int,vector<pair<int, int>>> adjList;
-        for( auto f : flights )
-            adjList[f[0]].push_back( { f[1], f[2] } );
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
         
-        // minHeap based on cost of distance from source
-        priority_queue< vector<int>, vector<vector<int>>, greater<vector<int>> > minHeap;
-        minHeap.push( { 0, src, 0 } ); // cost, vertex, hops
+        vector<pair<int,int>> adj[n];
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
+        vector<int> dist(n, INT_MAX);
         
-        vector<int> dist(n+1, INT_MAX); //to avoid TLE
+        for(auto i : flights){
+            adj[i[0]].push_back({i[1], i[2]});
+        }
+        pq.push({0, src, 0});
+        dist[src] = 0;
         
-        while( !minHeap.empty() ) {
-            auto t = minHeap.top(); minHeap.pop();
-            int cost = t[0];
-            int curr = t[1];
-            int stop = t[2];
-            
-            if( curr == dst )
-                return cost;
-            
-                //Optimization to avoid TLE
-            if(dist[curr]<stop) continue;
-                 dist[curr]=stop;
-
-            if(stop >K ) continue;
-            
-                for( auto next : adjList[curr] )
-                    minHeap.push( { cost+next.second, next.first, stop+1 });
+        while(!pq.empty()){
+            auto f = pq.top();
+            pq.pop();
+            int cost = f[0];
+            int city = f[1];
+            int hop = f[2];
+            if(city == dst) return cost;
+            if(dist[city] < hop) continue;
+            dist[city] = hop;
+            if(hop > k) continue;
+            for(auto i : adj[city]){
+                pq.push({i.second+cost, i.first, hop+1});
+            }
         }
         return -1;
     }
