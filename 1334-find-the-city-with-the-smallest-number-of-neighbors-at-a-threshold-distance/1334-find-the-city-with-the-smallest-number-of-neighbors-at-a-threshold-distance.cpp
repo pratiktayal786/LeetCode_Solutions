a61719262462a6e6100f4a358ad20a4062216aed
Pratik Tayal <77280501+pratiktayal786@users.noreply.github.com>
2023-06-17 14:56:38 +0530

    Time: 77 ms (93.20%), Space: 16.5 MB (8.87%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
-    using pii = pair<int,int>; 
+    
+    using pii = pair<int,int>;
     int dijkstra(int src, vector<pii> adj[], int n, int thre){
-        
-        vector<int>  dist(n+1, INT_MAX);
+        vector<int> dist(n, INT_MAX);
         priority_queue<pii, vector<pii>, greater<pii>> pq;
         int k = 0;
         
-        pq.push({0, src});
+        pq.push({0,src});
         dist[src] = 0;
         
         while(!pq.empty()){
-            auto [weight, i] = pq.top();
+            auto [weight, city] = pq.top();
             pq.pop();
-            if(dist[i] < weight) continue;
-            for(auto [j, w] : adj[i]){
-                if(dist[i]+w < dist[j]){
-                    dist[j] = dist[i]+w;
-                    pq.push({w,j});
+            
+            if(dist[city] < weight) continue;
+            
+            for(auto [c, w] : adj[city]){
+                if(dist[city]+w < dist[c]){
+                    dist[c] = dist[city]+w;
+                    pq.push({dist[c], c});
                 }
             }
         }
@@ -25,25 +27,39 @@ public:
         for(auto i : dist){
             if(i <= thre) k++;
         }
+        
         return k;
     }
+    
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
         
-        vector<pii> graph[n];
+        vector<pii> adj[n];
         vector<pii> ans;
         
-        for(auto i : edges){
-            graph[i[0]].push_back({i[1], i[2]});
-            graph[i[1]].push_back({i[0], i[2]});
+        for(auto city : edges){
+            adj[city[0]].push_back({city[1], city[2]});
+            adj[city[1]].push_back({city[0], city[2]});
         }
         
         for(int i = 0; i < n; i++){
-            ans.push_back({dijkstra(i, graph, n, distanceThreshold), i});
+            ans.push_back({i, dijkstra(i, adj, n, distanceThreshold)});
         }
         
         sort(ans.begin(), ans.end(), [] (auto a, auto b){
-            return a.first != b.first ? a.first < b.first : a.second > b.second;
+           return a.second != b.second ? a.second < b.second : a.first > b.first; 
         });
-        return ans[0].second;
+        
+        return ans[0].first;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
