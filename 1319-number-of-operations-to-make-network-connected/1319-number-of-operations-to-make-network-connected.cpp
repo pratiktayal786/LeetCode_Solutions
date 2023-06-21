3c0f911aef0464237d485794ec0a81d4dd0ff76a
Pratik Tayal <77280501+pratiktayal786@users.noreply.github.com>
2023-06-21 20:52:19 +0530

    Time: 138 ms (68.48%), Space: 41.8 MB (76.93%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,31 +1,34 @@
 class Solution {
 public:
-    void dfs(int i, vector<int> adj[], vector<int> &vis){
-        vis[i] = 1;
-        for(auto j : adj[i]){
-            if(!vis[j]){
-                dfs(j, adj, vis);
-            }
-        }
+    
+    vector<int> parent;
+    
+    int find(int x){
+        if(parent[x] == x) return x;
+        return parent[x] = find(parent[x]);
     }
+    
+    void dsUnion(int u, int v){
+        int pu = find(u);
+        int pv = find(v);
+        if(pu != pv) parent[pv] = pu;
+    }
+    
     int makeConnected(int n, vector<vector<int>>& connections) {
         
         if(connections.size() < n-1) return -1;
-        vector<int> vis(n, 0);
-        vector<int> adj[n];
+        parent.resize(n);
+        for(int i = 0; i < n; i++) parent[i] = i;
         
         for(auto i : connections){
-            adj[i[0]].push_back(i[1]);
-            adj[i[1]].push_back(i[0]);
+            int u = i[0];
+            int v = i[1];
+            dsUnion(u, v);
         }
         
-        int ans = 0;
-        
+        int ans = 0; 
         for(int i = 0; i < n; i++){
-            if(!vis[i]){
-                dfs(i, adj, vis);
-                ans++;
-            }
+            if(parent[i] == i) ans++;
         }
         return ans-1;
     }
