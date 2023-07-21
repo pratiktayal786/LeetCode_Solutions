0fa6147d688e9b6c8d08f831f4de03b6559ba99b
Pratik Tayal <77280501+pratiktayal786@users.noreply.github.com>
2023-07-21 23:41:26 +0530

    Time: 8 ms (57.78%), Space: 8.9 MB (47.26%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
     
-    int solve(int i, int j, vector<vector<int>> &arr, vector<vector<int>> &dp){
-                
-        int n = arr.size();
-        
-        if(i == n-1){
-            return arr[i][j];
-        }
-        
-        if(dp[i][j] != -1) return dp[i][j];
-        
-        int down = min(solve(i+1, j, arr, dp), 
-                                  solve(i+1, j+1, arr, dp));
-        
-        int diagonal = min(solve(i+1, j, arr, dp), 
-                                  solve(i+1, j+1, arr, dp));
-        return dp[i][j] = arr[i][j] + min(down, diagonal);
-    }
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        vector<vector<int>> dp(n, vector<int> (n, -1));
-        return solve(0, 0, triangle, dp);
+        vector<vector<int>> dp(n, vector<int> (n, 0));
+        
+        for(int j = 0; j < n; j++){
+            dp[n-1][j] = triangle[n-1][j];
+        }
+        
+        for(int i = n-2; i >= 0; i--){
+            for(int j = i; j >= 0; j--){
+                int down = dp[i+1][j];
+                int diagonal = dp[i+1][j+1];
+                dp[i][j] = triangle[i][j] + min(down, diagonal);
+            }
+        }
+        return dp[0][0];
     }
 };
