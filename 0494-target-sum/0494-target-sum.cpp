1b15f3269f2da57a14861d4768624e3f7c5e6bf5
Pratik Tayal <77280501+pratiktayal786@users.noreply.github.com>
2023-08-06 00:44:08 +0530

    Time: 12 ms (81.84%), Space: 12 MB (50.85%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,17 +1,43 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
+    int findWays(vector<int> &num, int tar){
+     int n = num.size();
+
+    vector<vector<int>> dp(n,vector<int>(tar+1,0));
     
-    int solve(int i, int total, int tar, vector<int> &arr){
-        if(total == tar && i == arr.size()) return 1;
-        if(i == arr.size()) return 0;
-        if(dp[i][total+1000] != -1) return dp[i][total+1000];
-        return dp[i][total+1000] = solve(i+1, total+arr[i], tar, arr) +
-            solve(i+1, total-arr[i], tar, arr);
+    if(num[0] == 0) dp[0][0] =2;  // 2 cases -pick and not pick
+    else dp[0][0] = 1;  // 1 case - not pick
+    
+    if(num[0]!=0 && num[0]<=tar) dp[0][num[0]] = 1;  // 1 case -pick
+    
+    for(int ind = 1; ind<n; ind++){
+        for(int target= 0; target<=tar; target++){
+            
+            int notTaken = dp[ind-1][target];
+    
+            int taken = 0;
+                if(num[ind]<=target)
+                    taken = dp[ind-1][target-num[ind]];
+        
+            dp[ind][target]= (notTaken + taken);
+        }
+    }
+    return dp[n-1][tar];
+}
+
+int countPartitions(int n, int d, vector<int>& arr){
+    int totSum = 0;
+    for(int i=0; i<n;i++){
+        totSum += arr[i];
     }
     
+    //Checking for edge cases
+    if(totSum-d <0 || (totSum-d)%2 ) return 0;
+    
+    return findWays(arr,(totSum-d)/2);
+}
+    
     int findTargetSumWays(vector<int>& nums, int target) {
-        dp.resize(nums.size()+1, vector<int> (2005, -1));
-        return solve(0, 0, target, nums);
+        return countPartitions(nums.size(), target, nums);
     }
 };
